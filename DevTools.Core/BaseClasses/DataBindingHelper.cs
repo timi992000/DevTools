3138f19148360b6f6f2dbdcd2803d1654ea4e9b5
timi992000 <77628993+timi992000@users.noreply.github.com>
2023-06-21 19:29:07 +0200

    Swappable datacontext better way

---
+++ DevTools.Core/BaseClasses/DataBindingHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace DevTools.Core.BaseClasses
+{
+	public class DataBindingHelper : Freezable
+	{
+		public static readonly DependencyProperty DataProperty;
+
+		static DataBindingHelper()
+		{
+			DataProperty = DependencyProperty.Register(nameof(Data), typeof(object), typeof(DataBindingHelper));
+		}
+		public object Data
+		{
+			get { return (object)GetValue(DataProperty); }
+			set { SetValue(DataProperty, value); }
+		}
+
+		protected override Freezable CreateInstanceCore()
+		{
+			return new DataBindingHelper();
+		}
+	}
+}
