5f1de7153e76f738f2118a5de7018539f77038f4
Secodity <118847092+Secodity@users.noreply.github.com>
2023-06-24 16:46:39 +0200

    Added functional configuration

---
+++ DevTools.Core/Exceptions/OperationFailedException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTools.Core.Exceptions;
+public class OperationFailedException : Exception
+{
+    public OperationFailedException(string operationName) : base($"The operation '{operationName}' failed.") { }
+}
