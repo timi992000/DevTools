97711662ec89bbb652dca5ac4f2468013fa34932
timi992000 <77628993+timi992000@users.noreply.github.com>
2023-07-01 01:36:14 +0200

    Save changes to config and update config entries

--- DevTools.Core/Configuration/DevToolsConfiguration.cs
+++ DevTools.Core/Configuration/DevToolsConfiguration.cs
@@ -1,5 +1,4 @@
-﻿using DevTools.Core.Exceptions;
-using DevTools.Core.Extender;
+﻿using DevTools.Core.Extender;
 using DevTools.Core.OperationResult;
 using System;
 using System.Configuration;
@@ -10,106 +9,107 @@ using Config = System.Configuration.Configuration;
 namespace DevTools.Core.Configuration;
 public class DevToolsConfiguration
 {
-    #region - needs -
-    private static readonly string USER_CONFIG_DIR = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "DevTools");
+  #region - needs -
+  private static readonly string USER_CONFIG_DIR = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "DevTools");
 
-    private static readonly string USER_CONFIG_PATH = Path.Combine(USER_CONFIG_DIR, "User.config");
-    private static readonly string DEFAULT_CONFIG_PATH = Path.Combine(Environment.CurrentDirectory, "App.config");
-    private static readonly string DEFAULT_USER_CONFIG = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<configuration>\n\t<appSettings>\n\t</appSettings>\n</configuration>";
-    #endregion
+  private static readonly string USER_CONFIG_PATH = Path.Combine(USER_CONFIG_DIR, "User.config");
+  private static readonly string DEFAULT_CONFIG_PATH = Path.Combine(Environment.CurrentDirectory, "App.config");
+  private static readonly string DEFAULT_USER_CONFIG = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<configuration>\n\t<appSettings>\n\t</appSettings>\n</configuration>";
+  #endregion
 
-    #region - ctor -
-    private DevToolsConfiguration()
+  #region - ctor -
+  private DevToolsConfiguration()
+  {
+    ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap();
+    fileMap.ExeConfigFilename = DEFAULT_CONFIG_PATH;
+
+    m_DefaultConfig = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+
+    if (!File.Exists(USER_CONFIG_PATH))
+      __CreateUserConfig();
+
+    ExeConfigurationFileMap userFileMap = new ExeConfigurationFileMap();
+    userFileMap.ExeConfigFilename = USER_CONFIG_PATH;
+    m_UserConfig = ConfigurationManager.OpenMappedExeConfiguration(userFileMap, ConfigurationUserLevel.None);
+  }
+  #endregion
+
+  #region - Instance -
+  private static DevToolsConfiguration? m_Configuration { get; set; }
+  public static DevToolsConfiguration Instance => m_Configuration ??= new DevToolsConfiguration();
+  #endregion
+
+  #region - properties -
+
+  #region - private properties -
+  private Config? m_DefaultConfig { get; set; } = null;
+  private Config? m_UserConfig { get; set; } = null;
+  #endregion
+
+  #endregion
+
+  #region - methods -
+
+  #region - public methods -
+
+  #region [Set]
+  public void Set(string key, string value)
+  {
+    ArgumentNullException.ThrowIfNull(key, nameof(key));
+    ArgumentNullException.ThrowIfNull(value, nameof(value));
+
+    if (key.IsNullEmptyOrWhitespace())
+      throw new ArgumentException("String can't be empty or only containing whitespaces.", nameof(key));
+
+    if (value.IsNullEmptyOrWhitespace())
+      throw new ArgumentException("String can't be empty or only containing whitespaces.", nameof(value));
+
+    if (m_UserConfig.AppSettings.Settings.AllKeys.Any(x => x.Equals(key)))
+      m_UserConfig.AppSettings.Settings.Remove(key);
+
+    m_UserConfig.AppSettings.Settings.Add(key, value);
+    m_UserConfig.Save();
+  }
+  #endregion
+
+  #region [Get]
+  public DevOperationResult<string> Get(string key)
+  {
+    ArgumentNullException.ThrowIfNull(key, nameof(key));
+
+    if (key.IsNullEmptyOrWhitespace())
+      throw new ArgumentException("String can't be empty or only containing whitespaces.", nameof(key));
+
+    if (!m_UserConfig.AppSettings.Settings.AllKeys.Any(x => x.IsEquals(key)))
     {
-        ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap();
-        fileMap.ExeConfigFilename = DEFAULT_CONFIG_PATH;
+      if (m_DefaultConfig is null)
+        throw new Exception("No configuration is present.");
 
-        m_DefaultConfig = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+      if (!m_DefaultConfig.AppSettings.Settings.AllKeys.Any(x => x.IsEquals(key)))
+        return DevOperationResult<string>.Fail($"The key '{key}' doesn't exists.");
 
-        if (!File.Exists(USER_CONFIG_PATH))
-            __CreateUserConfig();
-
-        ExeConfigurationFileMap userFileMap = new ExeConfigurationFileMap();
-        userFileMap.ExeConfigFilename = USER_CONFIG_PATH;
-        m_UserConfig = ConfigurationManager.OpenMappedExeConfiguration(userFileMap, ConfigurationUserLevel.None);
+      return DevOperationResult<string>.Ok(m_DefaultConfig.AppSettings.Settings[key].Value);
     }
-    #endregion
 
-    #region - Instance -
-    private static DevToolsConfiguration? m_Configuration { get; set; }
-    public static DevToolsConfiguration Instance => m_Configuration ??= new DevToolsConfiguration();
-    #endregion
+    return DevOperationResult<string>.Ok(m_UserConfig.AppSettings.Settings[key].Value);
+  }
+  #endregion
 
-    #region - properties -
+  #endregion
 
-    #region - private properties -
-    private Config? m_DefaultConfig { get; set; } = null;
-    private Config? m_UserConfig { get; set; } = null;
-    #endregion
+  #region - private methods -
 
-    #endregion
+  #region [__CreateUserConfig]
+  private void __CreateUserConfig()
+  {
+    if (!Directory.Exists(USER_CONFIG_DIR))
+      Directory.CreateDirectory(USER_CONFIG_DIR);
+    File.WriteAllText(USER_CONFIG_PATH, DEFAULT_USER_CONFIG);
+  }
+  #endregion
 
-    #region - methods -
+  #endregion
 
-    #region - public methods -
-
-    #region [Set]
-    public void Set(string key, string value)
-    {
-        ArgumentNullException.ThrowIfNull(key, nameof(key));
-        ArgumentNullException.ThrowIfNull(value, nameof(value));
-
-        if (key.IsNullEmptyOrWhitespace())
-            throw new ArgumentException("String can't be empty or only containing whitespaces.", nameof(key));
-
-        if (value.IsNullEmptyOrWhitespace())
-            throw new ArgumentException("String can't be empty or only containing whitespaces.", nameof(value));
-
-        if (m_UserConfig.AppSettings.Settings.AllKeys.Any(x => x.Equals(key)))
-            throw new KeyAlreadyExistsException(key);
-
-        m_UserConfig.AppSettings.Settings.Add(key, value);
-    }
-    #endregion
-
-    #region [Get]
-    public DevOperationResult<string> Get(string key)
-    {
-        ArgumentNullException.ThrowIfNull(key, nameof(key));
-
-        if (key.IsNullEmptyOrWhitespace())
-            throw new ArgumentException("String can't be empty or only containing whitespaces.", nameof(key));
-
-        if (!m_UserConfig.AppSettings.Settings.AllKeys.Any(x => x.IsEquals(key)))
-        {
-            if (m_DefaultConfig is null)
-                throw new Exception("No configuration is present.");
-
-            if (!m_DefaultConfig.AppSettings.Settings.AllKeys.Any(x => x.IsEquals(key)))
-                return DevOperationResult<string>.Fail($"The key '{key}' doesn't exists.");
-
-            return DevOperationResult<string>.Ok(m_DefaultConfig.AppSettings.Settings[key].Value);
-        }
-
-        return DevOperationResult<string>.Ok(m_UserConfig.AppSettings.Settings[key].Value);
-    }
-    #endregion
-
-    #endregion
-
-    #region - private methods -
-
-    #region [__CreateUserConfig]
-    private void __CreateUserConfig()
-    {
-        if (!Directory.Exists(USER_CONFIG_DIR))
-            Directory.CreateDirectory(USER_CONFIG_DIR);
-        File.WriteAllText(USER_CONFIG_PATH, DEFAULT_USER_CONFIG);
-    }
-    #endregion
-
-    #endregion
-
-    #endregion
+  #endregion
 
 }
--- DevTools/ViewModels/MainWindowViewModel.cs
+++ DevTools/ViewModels/MainWindowViewModel.cs
@@ -4,66 +4,57 @@ using DevTools.Core.Configuration;
 using DevTools.Core.Exceptions;
 using DevTools.Core.Extender;
 using MahApps.Metro.Controls;
-using Microsoft.Win32;
 using System;
 using System.Windows;
 
 namespace DevTools.ViewModels
 {
-	public class MainWindowViewModel : ViewModelBase
-	{
-		public static bool IsDarkMode { get; set; }
-		public MainWindowViewModel(MetroWindow mainWindow)
-		{
-			MainWindow = mainWindow;
-			__DetectWinThemeAndSetStart();
-		}
+  public class MainWindowViewModel : ViewModelBase
+  {
+    private const string DarkModeUserConfigKey = "DarkMode";
+
+    public static bool IsDarkMode { get; set; }
+    public MainWindowViewModel(MetroWindow mainWindow)
+    {
+      MainWindow = mainWindow;
+      __DetectWinThemeAndSetStart();
+    }
 
 
-		public static MetroWindow MainWindow { get; set; }
+    public static MetroWindow MainWindow { get; set; }
 
-		public bool DarkModeToggled
-		{
-			get => Get<bool>();
-			set
-			{
-				Set(value);
-				MainWindowViewModel.IsDarkMode = value;
-			}
-		}
+    public bool ConfigurationOpen
+    {
+      get => Get<bool>();
+      set => Set(value);
+    }
 
-		public bool ConfigurationOpen
-		{
-			get => Get<bool>();
-			set => Set(value);
-		}
+    public void Execute_SwitchTheme()
+    {
+      var currentTheme = ThemeManager.Current.DetectTheme();
+      var inverseTheme = ThemeManager.Current.GetInverseTheme(currentTheme);
+      ThemeManager.Current.ChangeTheme(Application.Current, inverseTheme);
+      DevToolsConfiguration.Instance.Set(DarkModeUserConfigKey, IsDarkMode ? "True" : "False");
+    }
+    private void __DetectWinThemeAndSetStart()
+    {
+      try
+      {
+        var DarkMode = DevToolsConfiguration.Instance.Get(DarkModeUserConfigKey).ValidateAndRetrieveResult();
+        var AccentColor = DevToolsConfiguration.Instance.Get("AccentColor").ValidateAndRetrieveResult();
+        ThemeManager.Current.ChangeTheme(Application.Current, $"{(DarkMode.Equals("True") ? "Dark" : "Light")}.{AccentColor}");
+        IsDarkMode = DarkMode.Equals("True");
+      }
+      catch (OperationFailedException ex)
+      {
+        ShowErrorMessage(ex);
+      }
+      catch (Exception ex)
+      {
+        ShowErrorMessage(ex);
+        Environment.Exit(1);
+      }
+    }
 
-		public void Execute_SwitchTheme()
-		{
-			var currentTheme = ThemeManager.Current.DetectTheme();
-			var inverseTheme = ThemeManager.Current.GetInverseTheme(currentTheme);
-			ThemeManager.Current.ChangeTheme(Application.Current, inverseTheme);
-		}
-		private void __DetectWinThemeAndSetStart()
-		{
-			try
-			{
-                var DarkMode = DevToolsConfiguration.Instance.Get("DarkMode").ValidateAndRetrieveResult();
-                var AccentColor = DevToolsConfiguration.Instance.Get("AccentColor").ValidateAndRetrieveResult();
-
-                ThemeManager.Current.ChangeTheme(Application.Current, $"{(DarkMode.Equals("True") ? "Dark" : "Light")}.{AccentColor}");
-				IsDarkMode = DarkMode.Equals("True");
-            } 
-			catch(OperationFailedException ex)
-			{
-				MessageBox.Show(ex.Message);
-			} 
-			catch(Exception ex)
-			{
-				MessageBox.Show(ex.ToString());
-				Environment.Exit(1);
-			}
-		}
-
-	}
+  }
 }
