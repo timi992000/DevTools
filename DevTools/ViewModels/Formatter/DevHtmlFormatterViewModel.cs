1ae98468fa3ba4ffbf74e0129dbdb3901c3a40d2
timi992000 <77628993+timi992000@users.noreply.github.com>
2023-07-26 23:41:55 +0200

    HTML Formatter

---
+++ DevTools/ViewModels/Formatter/DevHtmlFormatterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using AngleSharp.Html;
+using AngleSharp.Html.Parser;
+using DevTools.Core.Extender;
+using System;
+using System.IO;
+
+namespace DevTools.ViewModels.Formatter
+{
+    public class DevHtmlFormatterViewModel : DevFormatterViewModelBase
+    {
+        public override void FormatText()
+        {
+            try
+            {
+                var sw = new StringWriter();
+                new HtmlParser().ParseDocument(TextToFormat).ToHtml(sw, new PrettyMarkupFormatter());
+                ConvertedText = sw.ToString();
+            }
+            catch (Exception ex)
+            {
+                HasError = false;
+                HasError = true;
+                ConvertedText = (ex.InnerException ?? ex).ToStringValue();
+            }
+        }
+    }
+}
---
+++ DevTools/Views/Formatter/DevHtmlFormatterTabView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using DevTools.Core.Extender;
+using DevTools.ViewModels.Formatter;
+using System.Windows.Controls;
+
+namespace DevTools.Views.Formatter
+{
+    /// <summary>
+    /// Interaction logic for DevHtmlFormatterTabView.xaml
+    /// </summary>
+    public partial class DevHtmlFormatterTabView : UserControl
+    {
+        public DevHtmlFormatterTabView()
+        {
+            InitializeComponent();
+            DataContext = new DevHtmlFormatterViewModel();
+        }
+
+        private void __TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (e.Source is TextBox tb && DataContext is DevFormatterViewModelBase vm)
+                vm.TextToFormat = tb.Text.ToStringValue();
+        }
+    }
+}
